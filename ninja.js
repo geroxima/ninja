8916762cb3dc0dd9e7e5f52d7dead48f218e1647
geroxima <geroshima123@gmail.com>
2023-12-28 23:10:53 -0300

    [TAREA] sensei 🧙

--- ninja.js
+++ ninja.js
@@ -4,7 +4,6 @@ class Ninja {
         this.health = 10;    
         this.speed = 3;
         this.strength = 3;
-
     }
 
     sayName() {
@@ -12,10 +11,29 @@ class Ninja {
     }
 
     showStats () {
-        console.log(`${this.health}, ${this.speed}, ${this.strength}`);
+        console.log(` Health: ${this.health}, Speed: ${this.speed}, Strength: ${this.strength}`);
     }
 
     drinkSake () {
         this.health += 10;
     }
 } 
+
+class Sensei extends Ninja {
+    constructor (name) {
+        super(name);
+        this.wisdom = 10;
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+    }
+
+    speakWisdom () {
+        super.drinkSake();
+        console.log("Remember, even the great coders started with 'Hello World' and not 'Hello Perfect Code'. Perfection is overrated, but debugging skills are legendary!");
+    }
+}
+
+const sensei = new Sensei("Jiraiya");
+sensei.speakWisdom();
+sensei.showStats();
